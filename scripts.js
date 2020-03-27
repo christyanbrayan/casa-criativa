dbca1441aca65e24bb87bfe4a0037b09ed417d67
christyanbrayan <christyankev@gmail.com>
2020-03-27 13:27:55 -0300

    day three - fix modal

--- scripts.js
+++ scripts.js
@@ -3,4 +3,14 @@ function onOff() {
     .querySelector("#modal")
     .classList
     .toggle("hide")
+
+  document
+    .querySelector("body")
+    .classList
+    .toggle("hideScroll")
+
+  document
+    .querySelector("#modal")
+    .classList
+    .toggle("addScroll")
 }
