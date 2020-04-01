ec7cd86b76e57860b27b814ec120d8d1acb13d77
christyanbrayan <christyankev@gmail.com>
2020-04-01 15:39:21 -0300

    day four - update index with nunjucks

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 const express = require("express")
 const server = express()
 
+const ideas = [
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    title: "Cursos de Programação",
+    category: "Estudo",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    url: "https://rocketseat.com.br"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+    title: "Exercícios",
+    category: "Saúde",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed labore porro a eligendi esse vero",
+    url: "https://rocketseat.com.br"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+    title: "Meditação",
+    category: "Mentalidade",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    url: "https://rocketseat.com.br"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+    title: "Karaokê",
+    category: "Diversão em Família",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed labore porro a eligendi esse vero",
+    url: "https://rocketseat.com.br"
+  },
+]
+
 server.use(express.static("public"))
 
 const nunjucks = require("nunjucks")
@@ -10,7 +41,8 @@ nunjucks.configure("views", {
 })
 
 server.get("/", function(req, res) {
-  return res.render("index.html")
+
+  return res.render("index.html", { ideas })
 })
 
 server.get("/ideias", function(req, res) {
