9d91a9894ad2cf52711f78862ad900aa0fc90920
christyanbrayan <christyankev@gmail.com>
2020-04-13 20:51:21 -0300

    day five - update db.js

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const db = new sqlite3.Database('./ws.db')
 
 db.serialize(function() {
 
+  // Criar a tabela
   db.run(
     `CREATE TABLE IF NOT EXISTS ideas(
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,29 +14,32 @@ db.serialize(function() {
       link TEXT
     );
   `)
+  //
+  // Inserir dado na tabela
+  // const query = `
+  //     INSERT INTO ideas(
+  //       image,
+  //       title,
+  //       category,
+  //       description,
+  //       link
+  //     ) VALUES (?,?,?,?,?);
+  //   `
+  // const values = [
+  //   "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+  //   "Cursos de Programação",
+  //   "Estudo",
+  //   "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  //   "https://rocketseat.com.br"
+  // ]
 
-  const query = `
-      INSERT INTO ideas(
-        image,
-        title,
-        category,
-        description,
-        link
-      ) VALUES (?,?,?,?,?);
-    `
-  const values = [
-    "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    "Cursos de Programação",
-    "Estudo",
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    "https://rocketseat.com.br"
-  ]
+  // db.run(query, values, function(err) {
+  //   if (err) return console.log(err)
+  //
+  //   console.log(this)
+  // })
 
-  db.run(query, values, function(err) {
-    if (err) return console.log(err)
-
-    console.log(this)
-  })
+  // Deletar um dado da tabela
 
   // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
   //   if (err) return console.log(err)
@@ -43,10 +47,12 @@ db.serialize(function() {
   //   console.log("DELETADO", this)
   // })
 
-  db.all(`SELECT * FROM ideas`, function(err, rows) {
-    if (err) return console.log(err)
-
-    console.log(rows)
-  })
+  // Consultar dados na tabela
+  //
+  // db.all(`SELECT * FROM ideas`, function(err, rows) {
+  //   if (err) return console.log(err)
+  //
+  //   console.log(rows)
+  // })
 
 })
