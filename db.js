522e44fc900af53343d49fa7a58b66858ee30d84
christyanbrayan <christyankev@gmail.com>
2020-04-13 20:46:23 -0300

    day five - update db.js and node db.js

--- db.js
+++ db.js
@@ -31,17 +31,17 @@ db.serialize(function() {
     "https://rocketseat.com.br"
   ]
 
-  // db.run(query, values, function(err) {
-  //   if (err) return console.log(err)
-  //
-  //   console.log(this)
-  // })
-
-  db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
+  db.run(query, values, function(err) {
     if (err) return console.log(err)
 
-    console.log("DELETADO", this)
+    console.log(this)
   })
+
+  // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
+  //   if (err) return console.log(err)
+  //
+  //   console.log("DELETADO", this)
+  // })
 
   db.all(`SELECT * FROM ideas`, function(err, rows) {
     if (err) return console.log(err)
