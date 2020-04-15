071e6a052b40d0068f614c0bbd2e9026cabaeac0
christyanbrayan <christyankev@gmail.com>
2020-04-15 12:06:42 -0300

    day five - finish config form + database

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ const db = require("./db")
 
 server.use(express.static("public"))
 
+server.use(express.urlencoded({ extended: true }))
+
 const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
   express: server,
@@ -88,6 +90,36 @@ server.get("/ideias", function(req, res) {
     const reversedIdeas = [...rows].reverse()
     return res.render("ideias.html", { ideas: reversedIdeas })
   })
+})
+
+server.post("/", function(req, res) {
+  // Inserir dado na tabela
+  const query = `
+      INSERT INTO ideas(
+        image,
+        title,
+        category,
+        description,
+        link
+      ) VALUES (?,?,?,?,?);
+    `
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.category,
+    req.body.description,
+    req.body.link,
+  ]
+
+  db.run(query, values, function(err) {
+    if (err) {
+      console.log(err)
+      return res.send("Erro no banco de dados!")
+    }
+
+    return res.redirect("/ideias")
+  })
+
 })
 
 server.listen(3000)
